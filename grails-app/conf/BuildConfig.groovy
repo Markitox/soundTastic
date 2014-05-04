ca42e41e66caba5a46a3d56211fcefeb258d429b
Marco Antonio Muñiz Ochoa <marco.muniz.ochoa@gmail.com>
2014-05-04 15:27:03 -0500

    Changes based to new structure of Grails 2.3.5 and add coffeescript-compiler plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,64 +2,81 @@ grails.servlet.version = "3.0" // Change depending on target container complianc
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+    // configure settings for the test-app JVM, uses the daemon by default
+    test   : [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon: true],
+    // configure settings for the run-app JVM
+    run    : [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
+    // configure settings for the run-war JVM
+    war    : [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    checksums true // Whether to verify checksums on resolve
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+  // inherit Grails' default dependencies
+  inherits("global") {
+    // specify dependency exclusions here; for example, uncomment this to disable ehcache:
+    // excludes 'ehcache'
+  }
+  log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+  checksums true // Whether to verify checksums on resolve
+  legacyResolve false
+  // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
-    repositories {
-        inherits true // Whether to inherit repository definitions from plugins
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
-        mavenCentral()
+  repositories {
+    inherits true // Whether to inherit repository definitions from plugins
 
-        // uncomment these to enable remote dependency resolution from public Maven repositories
-        mavenCentral()
-        mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-        mavenRepo "http://maven.springframework.org/release"
-        mavenRepo "http://maven.springframework.org/snapshot"
-        mavenRepo "http://maven.springframework.org/milestone"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+    grailsPlugins()
+    grailsHome()
+    mavenLocal()
+    grailsCentral()
+    mavenCentral()
+    // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
+    //mavenRepo "http://repository.codehaus.org"
+    //mavenRepo "http://download.java.net/maven/2/"
+    //mavenRepo "http://repository.jboss.com/maven2/"
+    mavenRepo "http://maven.springframework.org/release"
+    mavenRepo "http://maven.springframework.org/snapshot"
+    mavenRepo "http://maven.springframework.org/milestone"
+  }
 
-//        runtime 'mysql:mysql-connector-java:5.1.16'
+  dependencies {
+    // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
+
+    // runtime 'mysql:mysql-connector-java:5.1.27'
 //        runtime 'org.springframework.social:spring-social-google:0.0.1-SNAPSHOT'
 //        runtime 'org.springframework.social:spring-social-facebook:1.0.0.RELEASE'
-        runtime 'de.u-mass:lastfm-java:0.1.2'
-    }
+    runtime 'de.u-mass:lastfm-java:0.1.2'
+  }
 
-    plugins {
-        // plugins for the build system only
-        build ":tomcat:7.0.50"
+  plugins {
+    // plugins for the build system only
+    build ":tomcat:7.0.50"
 
-        // plugins for the compile step
-        compile ":scaffolding:2.0.1"
-        compile ':cache:1.1.1'
+    // plugins for the compile step
+    compile ":scaffolding:2.0.1"
+    compile ':cache:1.1.1'
 
-        // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.7" // or ":hibernate4:4.1.11.6"
-        runtime ":database-migration:1.3.8"
-        runtime ":jquery:1.10.2.2"
-        runtime ":resources:1.2.1"
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0.1"
-        //runtime ":cached-resources:1.1"
-        //runtime ":yui-minify-resources:0.1.5"
-    }
+    // plugins needed at runtime but not for compilation
+    runtime ":hibernate:3.6.10.7" // or ":hibernate4:4.1.11.6"
+    runtime ":database-migration:1.3.8"
+    runtime ":jquery:1.10.2.2"
+    runtime ":resources:1.2.1"
+    compile ":coffeescript-compiler:0.9.4"
+
+    // Uncomment these (or add new ones) to enable additional resources capabilities
+    //runtime ":zipped-resources:1.0.1"
+    //runtime ":cached-resources:1.1"
+    //runtime ":yui-minify-resources:0.1.5"
+  }
 }
